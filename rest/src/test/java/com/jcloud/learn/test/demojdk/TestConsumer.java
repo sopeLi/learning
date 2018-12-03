38e104cab9ed822f34fcded6fce8e26d35a2611e
lixin10 <lixin10@qiyi.com>
2018-12-03 20:43:59 +0800

    update .gitgnore

---
+++ rest/src/test/java/com/jcloud/learn/test/demojdk/TestConsumer.java
@@ -0,0 +1,21 @@
+package com.jcloud.learn.test.demojdk;
+
+import java.util.function.Consumer;
+
+public class TestConsumer {
+
+    public void test(){
+        Consumer<String> consumer=s -> System.out.print(s+"?");
+        consumer.accept("李鑫!");
+    }
+
+    public void testConsumer1(String[] content,Consumer<String[]> consumer){
+       consumer.accept(content);
+    }
+
+    public static void main(String[] args) {
+        TestConsumer consumer=new TestConsumer();
+        String s[]={"name", "lixin"};
+        consumer.testConsumer1(s, kv -> System.out.println(kv[0]+":"+kv[1]));
+    }
+}
